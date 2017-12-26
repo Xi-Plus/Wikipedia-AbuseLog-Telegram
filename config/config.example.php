8702d8d42bfaf7a69a1ca0d269b63fdf50b0baf5
xiplus <huangxuanyuxiplus@gmail.com>
2017-12-26 21:38:01 +0800

    add blacklist

--- config/config.example.php
+++ config/config.example.php
@@ -3,14 +3,16 @@
 $C['token'] = 'token';
 $C['chat_id'] = 'chat_id';
 
-$C["wikiapi"] = "https://zh.wikipedia.org/w/api.php";
-$C["user"] = "";
-$C["pass"] = "";
-$C["cookiefile"] = __DIR__."/../data/cookie.txt";
-$C["User-Agent"] = "AbuseLogBot";
-$C["allowsapi"] = array("cli");
+$C['blacklist'] = [];
 
-$C["day"] = ["日", "一", "二", "三", "四", "五", "六"];
+$C['wikiapi'] = "https://zh.wikipedia.org/w/api.php";
+$C['user'] = "";
+$C['pass'] = "";
+$C['cookiefile'] = __DIR__."/../data/cookie.txt";
+$C['User-Agent'] = "AbuseLogBot";
+$C['allowsapi'] = array("cli");
+
+$C['day'] = ["日", "一", "二", "三", "四", "五", "六"];
 
 $C['defaultdata'] = array(
 	"lasttime" => date("Y-m-d", time()-60*10)."T".date("H:i:s", time()-60*10)."Z"
